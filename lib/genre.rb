d95c556aee228d7eac68d005693bebb49a5ec409
wei chen <weichen9083@gmail.com>
2020-12-10 02:06:32 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,26 +1,4 @@
 class Genre 
-  attr_accessor :name
-  @@all = []
-  
-  def self.all 
-    @@all
-  end 
-  
-  def initialize (name)
-    @name = name
-    @@all << self 
-  end 
-  
-  def songs 
-    Song.all.select do |songs| songs.genre == self
-  end 
-    
-  end 
-  
-  def artists
-    self.songs.collect do |song| song.artist 
-  end 
-    
-  end 
+ 
   
 end 
